6d49b0df6ac4c876a88cb56f48821e27f21450c9
darmis007 <darshmishra3010@gmail.com>
2020-05-01 17:23:33 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { useState,useEffect} from 'react';
+import Recipe from "./Recipe";
+import './App.css';
+import './index.css';
+const api = {
+  key: "0fcac4bd82cc4a97998283bfcbecdd0f",
+  base: "https://api.spoonacular.com/recipes/search?query=cheese&number=2"
+}
+
+const App = () => {
+  const [query, setQuery] = useState('Cheese');
+  const [search,setSearch] = useState("");
+  const [recipes,setRecipes]= useState([]);
+
+  useEffect( () => {
+    getRecipes();
+  },[query]);
+  
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${query}&apiKey=${api.key}`)
+    const data = await response.json();
+    setRecipes(data);
+    console.log(data);
+        }
+
+    const updateSearch = e => {
+      setSearch(e.target.value)
+    }
+    
+    const getSearch = e => {
+      e.preventDefault();
+      setQuery(search);
+
+      setSearch('');
+    }
+  return (
+    <div className="App">
+      <center>
+      <form onSubmit={getSearch} className="search-box">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
+        <br /><button className='search-button' type='submit'>
+          Submit
+        </button>
+      </form>
+      <ul>
+    
+      {recipes.map(recipe =>(
+        <li>
+          <a href="#">
+        <Recipe title={recipe.title} 
+        id={recipe.id}
+        readyInMinutes={recipe.readyInMinutes} 
+        image={recipe.image}
+        missedIngredients={recipe.missedIngredients}
+        usedIngredients={recipe.usedIngredients}
+         />
+         </a>
+         </li>
+      ))}
+      </ul>
+      </center>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Recipe.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+const api = {
+    key: "0fcac4bd82cc4a97998283bfcbecdd0f",
+    base: "https://api.spoonacular.com/recipes/search?query=cheese&number=2"
+  }
+
+  const Recipe = (props) => {
+    var id = 0
+    
+    const getRecipe = async (id) => {
+            console.log(props.id)
+      const response = await fetch(`https://api.spoonacular.com/recipes/${props.id}/information?includeNutrition=false&apiKey=${api.key}`)
+      const data = await response.json();
+      console.log(data)
+      window.open(data.sourceUrl, "_blank");
+      console.log(data.readyInMinutes);
+          
+        }
+        
+    
+        const updateId = e => {
+            id = props.id
+            getRecipe(id)
+          }
+  
+    return (
+        <div>
+            <main>
+                <div className="food-container">
+                <div className='food-title'><h1>{props.title}</h1>
+                </div>
+                <div className="food-ingredients-title">
+                <h2>Other Ingredients</h2>
+                </div>
+                <div className='missed-ingredients'>
+                {props.missedIngredients.map(ingredients =>(
+                <h4>{ingredients.originalString}</h4>
+                ))}
+                </div>
+                <img src={props.image} height='80%' width='70%' data-id= {props.id} onClick={updateId} alt="food"  />
+                </div>
+        </main>
+    </div>
+    )
+  }
+  
+
+
+
+
+/*
+const Recipe = (props) => {
+    return(
+        <div>
+            <h1>{props.title}</h1>
+      <h2>Missed Ingredients</h2>
+      {props.missedIngredients.map(ingredients =>(
+                <h4>{ingredients.originalString}</h4>
+      ))}
+            <img src={props.image} alt="food"  />
+            
+
+        </div>
+    )
+}
+*/
+export default Recipe;
